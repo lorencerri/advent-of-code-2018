19eb4925c9b716d1043581592e0308e20010bab6
Loren Cerri <truexpixels@gmail.com>
2018-12-01 01:19:04 -0800

    Update JavaScript-Part-1.js

--- Day 1/JavaScript-Part-1.js
+++ Day 1/JavaScript-Part-1.js
@@ -3,10 +3,10 @@ Day 1 - Chronal Calibration [Part 1]
 https://adventofcode.com
 
 Declare Input Here: */
-var input = '+1, +1, -2';
+var input = '+1, +1, -2'.split(', ');
 
 // Calculate
-input = input.split(', ').reduce((a, b) => (a + parseInt(b)), 0);
+input = input.reduce((a, b) => (a + parseInt(b)), 0);
 
 // Log
 console.log(input);
