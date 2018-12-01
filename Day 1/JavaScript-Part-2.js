90a34bbdc1a9c17693846600a9ea3b4978892a16
Loren Cerri <truexpixels@gmail.com>
2018-11-30 22:27:51 -0800

    Update JavaScript-Part-2.js

--- Day 1/JavaScript-Part-2.js
+++ Day 1/JavaScript-Part-2.js
@@ -5,33 +5,19 @@ https://adventofcode.com
 Declare Input Here: */
 var input = `+7, +7, -2, -7, -4`;
 
-// Declare Variables
-var frequencies, sum, i, found, sign, num;
-
-// Initialize Variables
-frequencies = [];
-sum = 0;
-i = 0;
+// Init Sum
+var sum = 0;
+var list = [];
+var found;
 input = input.split(', ');
 
+// Calc Loop
 while (!found) {
-
-  // Calculate Frequency
-  sign = input[i].substring(0, 1);
-  num = parseInt(input[i].substring(1, input[i].length));
-  if (sign == '-') sum -= num;
-  else sum += num;
-
-  // Check if it has been a frequency
-  if (frequencies.includes(sum)) {
-    console.log(sum);
-    found = true;
-  } else frequencies.push(sum); // Otherwise, push to list
-
-  // Increment
-  i++;
-
-  // Check if end of list, if so reset index
-  if (i == input.length) i = 0;
-
+  for (var i in input) {
+    if (list.includes(sum += parseInt(input[i]))) {
+      found = true;
+      console.log(list[list.length-1] + parseInt(input[i]));
+      break;
+    } else list.push(sum);
+  }
 }
