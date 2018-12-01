bc8431ac2f6a05d198fcfc8aee8684a57723d979
Loren Cerri <truexpixels@gmail.com>
2018-12-01 02:18:43 -0800

    Create Rust-Part-1.rs

---
+++ Day 1/Rust-Part-1.rs
@@ -0,0 +1,19 @@
+// Day 1 - Chronal Calibration [Part 1]
+// https://adventofcode.com
+
+fn main() {
+  
+  // Declare Input Here:
+  let input = "-1, +2, -3, +1, -4".split(", ");
+
+  let mut freq = 0;
+
+  // Iterate
+  for (_, element) in input.enumerate() {
+    freq += element.parse().unwrap();
+  } 
+
+  // Print Frequency
+  println!("{}", freq);
+  
+}
