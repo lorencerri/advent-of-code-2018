257149e0db5f0645a57fa99032729a8c31c581f8
Loren Cerri <truexpixels@gmail.com>
2018-12-01 02:25:26 -0800

    Update Python-Part-1.py

--- Day 1/Python-Part-1.py
+++ Day 1/Python-Part-1.py
@@ -3,7 +3,7 @@ from functools import reduce
 # Day 1 - Chronal Calibration [Part 1]
 # https://adventofcode.com
 #
-# Declare Input Here:
+# Input:
 input = '+1, +5, -2'.split(', ')
 
 # Calculate & Print
