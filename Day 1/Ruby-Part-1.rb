4b86958ecfd42c3e28700b70d3fbc443ba0eaa95
Loren Cerri <truexpixels@gmail.com>
2018-12-01 02:24:30 -0800

    Update Ruby-Part-1.rb

--- Day 1/Ruby-Part-1.rb
+++ Day 1/Ruby-Part-1.rb
@@ -1,7 +1,7 @@
 # Day 1 - Chronal Calibration [Part 1]
 # https://adventofcode.com
 #
-# Declare Input Here:
+# Input:
 input = '+1, +1, -2, -5'.split(', ')
 
 puts input.reduce(0) { |a, b| a + Integer(b) }
