60474b7af931f7ef58ebced7236995e0271da4fa
Loren Cerri <truexpixels@gmail.com>
2018-12-01 22:04:32 -0800

    Create JavaScript-Part-2.js

---
+++ Day 2/JavaScript-Part-2.js
@@ -0,0 +1,26 @@
+// Variables
+var input = 'INPUT_GOES_HERE'.split('\n')
+var best = { score: 0 };
+var bestStr = '';
+var strLen = 26;
+
+// Loop every element
+for (var i = 0; i < input.length; i++) {
+
+  // Filter out instances of same element
+  var testArray = input.filter(a => a !== input[i])
+  
+  // Score similarity of element
+  for (var x = 0; x < testArray.length; x++) {
+    var score = 0;
+    for (var y = 0; y < strLen; y++) if (testArray[x][y] === input[i][y]) score++;
+    if (score > best.score) best = { score: score, first: input[i], second: testArray[x]}
+  }
+
+}
+
+// Get same characters
+for (var i = 0; i < best.first.length; i++) if (best.first[i] == best.second[i]) bestStr += best.first[i];
+
+// Output puzzle answer
+console.log(bestStr);
