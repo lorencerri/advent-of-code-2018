60b1922dba7ff7d5269a3c02333911989522b1ce
Loren Cerri <truexpixels@gmail.com>
2018-12-04 23:01:20 -0800

    Update JavaScript.code.js

--- Day 5/JavaScript.code.js
+++ Day 5/JavaScript.code.js
@@ -1,19 +1,12 @@
-let polymers = '*abcdefghijklmnopqrstuvwxyz';
-let values = [];
-let str;
+let input = 'INPUT_GOES_HERE';
+let values = [], str, len, polymers = '*abcdefghijklmnopqrstuvwxyz';
 
 for (var x = 0; x < polymers.length; x++) {
     str = input.replace(new RegExp(`[${polymers[x]}]`, 'gi'), '');
-    remaining = true
-    while (remaining) {
-        remaining = false;
-        for (var i = 0; i < str.length - 1; i++) {
-            if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) {
-                str = str.substr(0, i - 1) + str.substr(i + 1);
-                remaining = true;
-            }
-        }
-    }
+    do { // Loop until no pairs remaining
+        len = str.length;
+        for (var i = 0; i < str.length-1; i++) if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) str = str.substr(0, i - 1) + str.substr(i + 1);
+    } while (len !== str.length);
     values.push(str.length);
 }
 
