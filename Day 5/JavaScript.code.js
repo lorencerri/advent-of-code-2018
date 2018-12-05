98a3ff004366b16eb753a9d5265c2c5ef041bead
Loren Cerri <truexpixels@gmail.com>
2018-12-05 01:21:42 -0800

    Update JavaScript.code.js

--- Day 5/JavaScript.code.js
+++ Day 5/JavaScript.code.js
@@ -1,5 +1,6 @@
 let input = 'INPUT_GOES_HERE';
 let values = [], str, len, l, polymers = '*abcdefghijklmnopqrstuvwxyz';
+
 let regex1 = new RegExp(`[${polymers[x]}]`, 'gi');
 let regex2 = new RegExp('(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)', 'g');
 
