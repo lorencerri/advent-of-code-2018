29b7663bbc996ea1612ee312cfca0b15c6875751
Loren Cerri <truexpixels@gmail.com>
2018-12-04 22:18:41 -0800

    Create JavaScript.code.js

---
+++ Day 5/JavaScript.code.js
@@ -0,0 +1,22 @@
+let polymers = '*abcdefghijklmnopqrstuvwxyz';
+let values = [];
+let str;
+
+for (var x = 0; x < polymers.length; x++) {
+    str = input.replace(new RegExp(`[${polymers[x]}]`, 'gi'), '');
+    remaining = true
+    while (remaining) {
+        remaining = false;
+        for (var i = 0; i < str.length - 1; i++) {
+            if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) {
+                str = str.substr(0, i - 1) + str.substr(i + 1);
+                remaining = true;
+                break;
+            }
+        }
+    }
+    values.push(str.length);
+}
+
+console.log('Puzzle #1:', values.shift());
+console.log('Puzzle #2:', Math.min(...values));
