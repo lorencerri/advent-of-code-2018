64e1e04fa550e1b453e7aff4c050eb09d9ba9a9d
Loren Cerri <truexpixels@gmail.com>
2018-12-04 23:00:07 -0800

    Update JavaScript.js

--- Day 5/JavaScript.js
+++ Day 5/JavaScript.js
@@ -2,35 +2,22 @@
 console.time('Timer');
 
 // Variables
-let input = 'INPUT_GOES_HERE';
 let polymers = '*abcdefghijklmnopqrstuvwxyz';
+let input = 'INPUT_GOES_HERE';
 let values = [];
 let str;
+let len;
 
 // Iterate through every character
 for (var x = 0; x < polymers.length; x++) {
 
     // Replace all of a certain character
     str = input.replace(new RegExp(`[${polymers[x]}]`, 'gi'), '');
-    
-    // Reset remaining variable
-    remaining = true;
 
-    // Loop until no pairs remaining
-    while (remaining) {
-        remaining = false;
-
-        // Iterate until a pair is found
-        for (var i = 0; i < str.length-1; i++) {
-            if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) {
-
-                // Update String
-                str = str.substr(0, i - 1) + str.substr(i + 1);
-                remaining = true;
-
-            }
-        }
-    }
+    do { // Loop until no pairs remaining
+        len = str.length;
+        for (var i = 0; i < str.length-1; i++) if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) str = str.substr(0, i - 1) + str.substr(i + 1);
+    } while (len !== str.length);
 
     // Update Results
     values.push(str.length);
