90f55a92dbfa2b351253c4b883d3950bfaf8f4f3
Loren Cerri <truexpixels@gmail.com>
2018-12-05 01:47:35 -0800

    Update JavaScript.code.js

--- Day 5/JavaScript.code.js
+++ Day 5/JavaScript.code.js
@@ -1,5 +1,5 @@
 let input = 'INPUT_GOES_HERE';
-let values = [], str, len, l, polymers = ' abcdefghijklmnopqrstuvwxyz';
+let values = [], str, len, polymers = ' abcdefghijklmnopqrstuvwxyz';
 let regex = new RegExp('(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)', 'g');
 
 for (var x = 0; x < polymers.length; x++) {
