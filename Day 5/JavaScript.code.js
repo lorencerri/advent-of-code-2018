a1c72b464f1b139b9fb70b5c03ec458e8793fcb8
Loren Cerri <truexpixels@gmail.com>
2018-12-04 23:01:38 -0800

    Update JavaScript.code.js

--- Day 5/JavaScript.code.js
+++ Day 5/JavaScript.code.js
@@ -3,7 +3,7 @@ let values = [], str, len, polymers = '*abcdefghijklmnopqrstuvwxyz';
 
 for (var x = 0; x < polymers.length; x++) {
     str = input.replace(new RegExp(`[${polymers[x]}]`, 'gi'), '');
-    do { // Loop until no pairs remaining
+    do {
         len = str.length;
         for (var i = 0; i < str.length-1; i++) if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) str = str.substr(0, i - 1) + str.substr(i + 1);
     } while (len !== str.length);
