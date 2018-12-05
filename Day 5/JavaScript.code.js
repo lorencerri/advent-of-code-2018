7f693ea617d94566847611d3315c1303cf319e71
Loren Cerri <truexpixels@gmail.com>
2018-12-05 01:19:45 -0800

    Update JavaScript.code.js

--- Day 5/JavaScript.code.js
+++ Day 5/JavaScript.code.js
@@ -1,16 +1,17 @@
 let input = 'INPUT_GOES_HERE';
-let values = [], str, len, polymers = '*abcdefghijklmnopqrstuvwxyz';
+let values = [], str, len, l, polymers = '*abcdefghijklmnopqrstuvwxyz';
+let regex1 = new RegExp(`[${polymers[x]}]`, 'gi');
+let regex2 = new RegExp('(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)', 'g');
 
 for (var x = 0; x < polymers.length; x++) {
-    str = input.replace(new RegExp(`[${polymers[x]}]`, 'gi'), '');
+    str = input.replace(regex1, '');
     do {
         len = str.length;
-        for (var i = 0; i < str.length-1; i++) if (Math.abs(str.charCodeAt(i - 1) - str.charCodeAt(i)) == 32) str = str.substr(0, i - 1) + str.substr(i + 1);
+        str = str.replace(regex2, '');
     } while (len !== str.length);
     values.push(str.length);
 }
 
+// Logging
 console.log('Puzzle #1:', values.shift());
 console.log('Puzzle #2:', Math.min(...values));
-
-// Currently o(n^2), use a stack data structure to improve performance
